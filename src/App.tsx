dc897d638da8801d0e4d2df5a3055637862fd653
penguinboots <sabrina.ynw@gmail.com>
2023-06-26 20:18:42 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.scss'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Placeholder from './components/Placeholder'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Hero />
+      <Placeholder />
+    </>
+  )
+}
+
+export default App
