28b46d8e6be2809aa833a6942e5274f80c899a3e
penguinboots <sabrina.ynw@gmail.com>
2023-06-27 14:51:23 -0700

    adjust key notation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -56,14 +56,14 @@ export default function Hero() {
     for (let i = 0; i < paths.length; i++) {
       const path = paths[i];
       const length = path.getTotalLength();
-      path.style["stroke-dashoffset"] = `${length}px`;
-      path.style["stroke-dasharray"] = `${length}px`;
-      path.style["stroke-width"] = `${strokeWidth}px`;
-      path.style["stroke"] = `${strokeColor}`;
+      path.style.strokeDashoffset = `${length}px`;
+      path.style.strokeDasharray = `${length}px`;
+      path.style.strokeWidth = `${strokeWidth}px`;
+      path.style.stroke = `${strokeColor}`;
       path.style[
         "animation"
       ] = `${duration}s svg-text-anim ${mode} ${timingFunction}`;
-      path.style["animation-delay"] = `${i * delay}s`;
+      path.style.animationDelay = `${i * delay}s`;
     }
   }
   setTextAnimation(0.1, 2.7, 1, "linear", "#ffffff", false);
