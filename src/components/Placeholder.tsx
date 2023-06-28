f960fd5b5a5072759f3f546726563470ba73f593
penguinboots <sabrina.ynw@gmail.com>
2023-06-27 22:06:41 -0700

    update placeholder

--- src/components/Placeholder.tsx
+++ src/components/Placeholder.tsx
@@ -1,5 +1,14 @@
-export default function Placeholder() {
+import wave from "../assets/bottom_wave.svg";
+import { ReactSVG } from "react-svg";
+
+
+export default function Placeholder(props) {
+  const colour = props;
   return (
-    <div className="placeholder">Placeholder</div>
-  )
+    <div className="placeholder" style={{backgroundColor: colour}}>
+      <ReactSVG
+        src={wave}
+      />
+    </div>
+  );
 }
