4a5eaef7e78f24c8834bf3306cb1ffdab4b3fa91
penguinboots <sabrina.ynw@gmail.com>
2023-06-28 16:10:39 -0700

    add navbar scroll behaviour

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-scroll";
 import { Squash as Hamburger } from "hamburger-react";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 interface NavbarProps {
   menuOpen: boolean;
@@ -9,8 +9,26 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = (props) => {
   const { menuOpen, setMenuOpen } = props;
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      const isVisible = prevScrollPos > currentScrollPos;
+
+      setPrevScrollPos(currentScrollPos);
+      setVisible(isVisible);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos]);
   return (
-    <div className="nav-wrapper">
+    <div className={`nav-wrapper ${visible ? "" : "hidden"}`}>
       <nav className="nav-bar">
         <div className="left-nav">
           <div className="logo">
@@ -25,4 +43,4 @@ const Navbar: React.FC<NavbarProps> = (props) => {
   );
 };
 
-export default Navbar
+export default Navbar;
