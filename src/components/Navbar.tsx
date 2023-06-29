271e67735529e878e9c3e546ef148b414faf323c
penguinboots <sabrina.ynw@gmail.com>
2023-06-29 16:14:36 -0700

    add responsive colour

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,14 +11,22 @@ const Navbar: React.FC<NavbarProps> = (props) => {
   const { menuOpen, setMenuOpen } = props;
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
+  const [navColor, setNavColor] = useState<string>("rgb(255, 255, 255, 0.8)");
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollPos = window.scrollY;
       const isVisible = prevScrollPos > currentScrollPos;
+      const threshold = 1700;
 
       setPrevScrollPos(currentScrollPos);
       setVisible(isVisible);
+
+      if (currentScrollPos <= threshold) {
+        setNavColor("rgb(255, 255, 255, 0.8)");
+      } else {
+        setNavColor("rgb(0, 0, 0, 0.7)");
+      }
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -27,17 +35,24 @@ const Navbar: React.FC<NavbarProps> = (props) => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, [prevScrollPos]);
+
   return (
     <div className={`nav-wrapper ${visible ? "" : "hidden"}`}>
       <nav className="nav-bar">
         <div className="left-nav">
           <div className="logo">
-            <Link to="app-wrapper" spy={true} smooth={true} duration={1000}>
+            <Link
+              to="app-wrapper"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              style={{ color: navColor }}
+            >
               SW
             </Link>
           </div>
         </div>
-        <Hamburger toggled={menuOpen} toggle={setMenuOpen} />
+        <Hamburger toggled={menuOpen} toggle={setMenuOpen} color={navColor}/>
       </nav>
     </div>
   );
