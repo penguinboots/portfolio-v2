c734e9ea9a46138986dbb9950aef8a94cc73c679
penguinboots <sabrina.ynw@gmail.com>
2023-07-01 12:24:18 -0700

    style call button

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,8 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPhone,
-  faPhoneVolume,
-} from "@fortawesome/free-solid-svg-icons";
+import { faPhone, faPhoneVolume } from "@fortawesome/free-solid-svg-icons";
 import { ReactSVG } from "react-svg";
 import glow from "../assets/glow_balls.svg";
 import ContactIcons from "./ContactIcons";
@@ -17,7 +14,7 @@ export default function CallToAction() {
         <h1>I think we'd be a perfect match...</h1>
         <button
           className={`phone-container ${ringing ? "ringing" : "open"}`}
-          onClick={() => setRinging(false)}
+          onClick={() => setRinging((prev) => !prev)}
         >
           {ringing ? (
             <div className="phone-ring">
@@ -25,12 +22,15 @@ export default function CallToAction() {
             </div>
           ) : (
             <>
-              <FontAwesomeIcon
-                icon={faPhoneVolume}
-                fade
-                className="phone-noring"
-              />
-
+              <div className="phone-ring spin-out">
+                <FontAwesomeIcon icon={faPhone} />
+              </div>
+              <div>
+                <FontAwesomeIcon
+                  icon={faPhoneVolume}
+                  className="phone-noring fade-in-late"
+                />
+              </div>
               <div className="call-text">
                 <h3>CALL ME MAYBE?</h3>
                 <h3>778 989 4856</h3>
